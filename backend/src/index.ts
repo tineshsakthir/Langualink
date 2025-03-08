8ed55315c0f930fd97a7323c7fa668f49d41b266
TINESH SAKTHI R <112597619+tineshsakthir@users.noreply.github.com>
2025-03-08 18:58:28 +0530

    Langualink first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Socket } from "socket.io";
+import http from "http";
+
+import express from 'express';
+import { Server } from 'socket.io';
+import { UserManager } from "./managers/UserManger";
+
+const app = express();
+const server = http.createServer(http);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const userManager = new UserManager();
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  socket.on("user-info", (name: string, language: string) => {
+    userManager.addUser(name, socket, language);
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    userManager.removeUser(socket.id);
+  })
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef, useState } from "react"
+import { Link } from "react-router-dom";
+import { Room } from "./Room";
+
+export const Landing = () => {
+    const [name, setName] = useState("");
+    const [language, setLanguage] = useState("");
+    const [localAudioTrack, setLocalAudioTrack] = useState<MediaStreamTrack | null>(null);
+    const [localVideoTrack, setlocalVideoTrack] = useState<MediaStreamTrack | null>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const [joined, setJoined] = useState(false);
+
+    const getCam = async () => {
+        const stream = await window.navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        })
+        // MediaStream
+        const audioTrack = stream.getAudioTracks()[0]
+        const videoTrack = stream.getVideoTracks()[0]
+        setLocalAudioTrack(audioTrack);
+        setlocalVideoTrack(videoTrack);
+        if (!videoRef.current) {
+            return;
+        }
+        videoRef.current.srcObject = new MediaStream([videoTrack])
+        videoRef.current.play();
+        // MediaStream
+    }
+
+    useEffect(() => {
+        if (videoRef && videoRef.current) {
+            getCam()
+        }
+    }, [videoRef]);
+
+    if (!joined) {
+
+        return <div>
+            <video autoPlay ref={videoRef}></video>
+            <input type="text" onChange={(e) => {
+                setName(e.target.value);
+            }}>
+            </input>
+
+            <p>Select a Room:</p>
+            {["Tamil", "English", "Spanish", "Arabic", "Hindi"].map((room) => (
+                <button
+                    key={room}
+                    onClick={() => setLanguage(room)}
+                    style={{
+                        backgroundColor: name === room ? "blue" : "white",
+                        color: name === room ? "white" : "black",
+                        padding: "8px 16px",
+                        margin: "5px",
+                        border: "1px solid black",
+                        borderRadius: "5px",
+                        cursor: "pointer",
+                    }}
+                >
+                    {room}
+                </button>
+            ))}
+            <button onClick={() => {
+                setJoined(true);
+            }}>Join</button>
+        </div>
+    }
+
+    return <Room name={name} language={language} localAudioTrack={localAudioTrack} localVideoTrack={localVideoTrack} />
+}
